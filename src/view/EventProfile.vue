e53172c93df4628556c64705559050ca6f3a2808
Gongzai Li <gli65@uclive.ac.nz>
2021-05-11 16:23:21 +1200

    update the even profile

--- src/view/EventProfile.vue
+++ src/view/EventProfile.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="row py-5 px-4">
-
+  <div class="row py-5 px-4" v-show="!loading">
+    <!--loading-->
 
     <div class="col-md-7  mx-auto">
       <!-- Profile widget -->
       <div class="bg-white shadow rounded overflow-hidden ">
 
         <div class="px-4 pt-2 pb-4 cover">
-          <img src="../assets/event-default.jpg" class="card-img" alt="...">
+          <img :src="eventImage" class="card-img" alt="event" @error="setEventImageDefault"/>
         </div>
 
 
@@ -17,72 +17,72 @@
             <div class="profile mr-3">
               <div class="demo-image">
                 <div class="block">
-                  <el-image
-                    style="width: 100px; height: 100px"
-                    :src="require('../assets/profile-default.png')"
-                    fit="scale-down"/>
-                  <h2 class="mt-0 mb-0">FName</h2>
-                  <h2 class="mt-0 mb-0">Lname</h2>
+                  <img :src="userImage" alt="userImage" width="120" class="rounded mb-2 img-thumbnail"
+                       @error="setUserImageDefault">
+                  <h2 class="mt-0 mb-0">{{ event.organizerFirstName }}</h2>
+                  <h2 class="mt-0 mb-0">{{ event.organizerLastName }}</h2>
                 </div>
               </div>
             </div>
           </div>
           <div class="align-items-start align-content-center col-md-8 border-left mt-0">
-            <h2> Title </h2>
-            <h4 class="mt-0 mb-0"><i class="el-icon-date"/> Date: </h4>
-            <h4 class="mt-0 mb-0"><i class="el-icon-receiving"/> Categories: </h4>
-            <h4 class="mt-0 mb-0"><i class="el-icon-user"/> Capacity: </h4>
-            <h4 class="mt-0 mb-0"><i class="el-icon-ship"/> Attendees: </h4><!-- only accepted-->
-            <h4 class="mt-0 mb-0"><i class="el-icon-loading"/> Url: </h4>
-            <h4 class="mt-0 mb-0"><i class="el-icon-place"/> Venue: </h4>
-            <h4 class="mt-0 mb-0"><i class="el-icon-money"/> Fee: </h4>
+            <h4> {{ event.title }} </h4>
+            <hr/>
+            <h5 class="mt-0 mb-1"><i class="el-icon-date"/> Date: {{ event.date }}</h5>
+            <h5 class="mt-0 mb-1"><i class="el-icon-user"/> Capacity: {{ event.capacity }}</h5>
+            <h5 class="mt-0 mb-1"><i class="el-icon-ship"/> Attendees: {{ event.attendeeCount }}</h5>
+            <!-- only accepted-->
+            <h5 class="mt-0 mb-1" v-show="event.url && event.url.length"><i class="el-icon-loading"/> Url:
+              {{ event.url }}</h5>
+            <h5 class="mt-0 mb-1"><i class="el-icon-place"/> Venue: {{ event.venue }}</h5>
+            <h5 class="mt-0 mb-1"><i class="el-icon-money"/> Fee: {{ event.fee > 0 ? event.fee : "Free" }}</h5>
           </div>
-
 
         </div>
         <div class="px-4 py-3">
+          <h5 class="mb-0"><i class="el-icon-receiving"/>Categories</h5>
+          <div class="p-4 rounded shadow-sm bg-light">
+            <p style="text-align: justify;">{{ categoriesResultList }}</p>
+          </div>
+        </div>
+
+        <div class="px-4 py-3">
           <h5 class="mb-0">Description</h5>
           <div class="p-4 rounded shadow-sm bg-light">
-            <p style="text-align: justify;">there are many description</p>
+            <p style="text-align: justify;">{{ event.description }}</p>
           </div>
         </div>
         <div class="py-4 px-4">
           <div class="d-flex align-items-center justify-content-between mb-3">
-            <h5 class="mb-0">Attendees List</h5>
+            <h5 class="mb-0">Attendees</h5>
           </div>
 
           <el-table
-            :data="tableData"
-            style="width: 100%"
-            :default-sort="{prop: 'date', order: 'descending'}"
-            max-height="250"
+              :data="tableData"
+              style="width: 100%"
+              :default-sort="{prop: 'date', order: 'descending'}"
+              max-height="250"
           >
-            <el-table-column min-width="55" prop="image" label="Image">
+            <el-table-column min-width="50" prop="image" label="Image">
               <template v-slot="scope">
-                <el-image
-                  style="width: 50px; height: 50px"
-                  :src="scope.row.image"
-                  fit="scale-down"/>
+                <img class="rounded" :src="scope.row.image" width="50" height="50" alt="user"
+                     @error="setUserImageDefault"/>
+                <!--                <el-image-->
+                <!--                    style="width: 50px; height: 50px"-->
+                <!--                    :src="scope.row.image"-->
+                <!--                    fit="scale-down"/>-->
               </template>
-
             </el-table-column>
             <el-table-column
-              prop="date"
-              label="日期"
-              sortable
-              width="180">
-            </el-table-column>
+                prop="firstName"
+                label="First Name"
+                sortable
+                width="250" />
             <el-table-column
-              prop="name"
-              label="姓名"
-              sortable
-              width="180">
-            </el-table-column>
-            <el-table-column
-              prop="address"
-              label="地址"
-              :formatter="formatter">
-            </el-table-column>
+                prop="lastName"
+                label="Last Name"
+                sortable
+                width="250" />
           </el-table>
         </div>
       </div>
@@ -92,37 +92,122 @@
 </template>
 
 <script>
+
 export default {
   name: "event-profile", //todo ask 8
   data() {
     return {
-      tableData: [{
-        image: require('../assets/profile-default.png'),
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        image: require('../assets/profile-default.png'),
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        image: require('../assets/profile-default.png'),
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        image: require('../assets/profile-default.png'),
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      eventId: null,
+      tableData: [],
+      userImage: '',
+      eventImage: '',
+      event: {
+        eventId: 0,
+        title: "",
+        categories: [],
+        organizerFirstName: "Scott",
+        organizerLastName: "Li",
+        attendeeCount: 0,
+        capacity: null,
+        description: "",
+        organizerId: null,
+        date: "2012-04-23T18:25:43.511Z",//todo should change
+        isOnline: false,
+        url: "",
+        venue: "",
+        requiresAttendanceControl: 0,
+        fee: null
+      },
+      organizer: {
+        firstName: "",
+        lastName: "",
+      },
+      categoriesAllTypes: [],
+      categoriesResultList: [],
+      error: {
+        eventError: '',
+        eventImageError: '',
+        userImageError: ''
+      },
+      loading: true
     }
   },
+  mounted() {
+    this.eventId = this.$route.params.eventId;
+    this.initEventProfile();
+  },
   methods: {
-    formatter(row,) {
-      return row.address;
-    }
+    initEventProfile: async function () {
+      await this.getCategories();
+      await this.getEvent();
+      await this.getEventAttendees();
+      this.setUpCategoriesTypes();
+    },
+    getEvent: async function () {
+      await this.$api.getEvent(this.eventId)
+          .then((response) => {
+            this.event = response.data;
+          })
+          .then(() => {
+            this.eventImage = this.$api.getEventImage(this.eventId);
+          })
+          .then(() => {
+            this.userImage = this.$api.getUserImage(this.event.organizerId);
+            this.loading = false;
+          })
+          .catch((error) => {
+            this.error.eventError = error.message;
+            this.loading = false;
+          })
+
+
+    },
+    getCategories: async function () {
+      await this.$api.getEventCategories()
+          .then((response) => {
+            this.categoriesAllTypes = response.data;
+          })
+          .catch((error) => {
+            alert(error.message);
+          })
+    },
+    getEventAttendees: async function () {
+      await this.$api.getEventAttendees(this.eventId)
+          .then((response) => {
+            this.setUpAttendeesTable(response.data);
+          })
+          .catch((error) => {
+            console.log(error.message);
+          })
+    },
+    setUpCategoriesTypes: function () {
+      this.categoriesResultList = [];
+      this.event.categories.forEach((id) => {
+        this.categoriesAllTypes.forEach((category) => {
+          if (id === category.id) {
+            this.categoriesResultList.push(category.name);
+          }
+        });
+      })
+    },
+    setUserImageDefault: function (e) {
+      e.target.src = require('../assets/profile-default.png');
+    },
+    setEventImageDefault: function (e) {
+      e.target.src = require('../assets/event-default.jpg');
+    },
+    setUpAttendeesTable: function (users) {
+      this.tableData = [];
+      let tableItem = {};
+      users.forEach((user) => {
+        tableItem.image = this.$api.getUserImage(user.attendeeId);
+        tableItem.firstName = user.firstName;
+        tableItem.lastName = user.lastName;
+        this.tableData.push(tableItem);
+        tableItem = {};
+      })
+
+    },
   }
 }
 </script>
